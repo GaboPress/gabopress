01082c1cb616a60199535f151e253512ba29b21a
andres-root <andres.root.coder@gmail.com>
2014-12-22 23:27:36 -0500

    added admin

--- cms/admin.py
+++ cms/admin.py
@@ -1,3 +1,14 @@
+# Django Modules
 from django.contrib import admin
 
-# Register your models here.
+# App Modules
+from .models import Article, Category, Tag
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Article)
+admin.site.register(Category)
+admin.site.register(Tag)
