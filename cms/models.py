647062d6e00f678d4993ffc6db033f614f2cd2fb
andres-root <andres.root.coder@gmail.com>
2014-12-22 22:43:51 -0500

    adding modles

--- cms/models.py
+++ cms/models.py
@@ -1,13 +1,51 @@
+# Django Modules
 from django.db import models
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=140, blank=False, null=False)
+    slug = models.SlugField(max_length=140, unique=True)
+    active = models.BooleanFiel(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['-created']
 
 
-class Post(models.Model):
-    pass
+class ArticleQuerySet(models.QuerySet):
+
+    def published(self):
+        return self.filter(publish=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=140, blank=False, null=False)
+    slug = models.SlugField(max_length=140, unique=True)
+    content = models.TextField()
+    publish = models.BooleanFiel(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+    objects = ArticleQuerySet.as_manager()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Article'
+        verbose_name_plural = 'Articles'
+        ordering = ['-created']
 
 
 class Comment(models.Model):
+    pass
+
+
+class Link(models.Model):
     pass
