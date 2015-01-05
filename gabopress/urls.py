3de78e8befbdbf973c1f751b23ce28f42b2d022e
andres-root <andres.root.coder@gmail.com>
2015-01-04 19:06:37 -0500

    setting up main urls

--- gabopress/urls.py
+++ gabopress/urls.py
@@ -1,8 +1,11 @@
+# Django Modules
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = patterns(
     '',
     url(r'^$', include('cms.urls')),
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
