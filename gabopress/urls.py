516ea68a2e29dcef471a1a7af1477c6026d58eb4
andres-root <andres.root.coder@gmail.com>
2015-01-06 20:43:21 -0500

    added urls

--- gabopress/urls.py
+++ gabopress/urls.py
@@ -9,6 +9,15 @@ urlpatterns = patterns(
     '',
     url(r'^$', include('cms.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'index', name='index'),
+    url(r'^article/(?P<slug>[\w-]+)/$', 'article', name='view_article'),
+    url(r'^(?P<year>\d{4})/$', 'list', name="year_archive"),
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'list', name="month_archive"),
+    url(r'^tag/(?P<tag>[\w-]+)/$', 'list', name='tag_archive'),
+    url(r'^author/(?P<author>[\w-]+)/$', 'list', name='author_archive'),
+    url(r'^confirm/comment/$', 'comment_posted', name='comment_posted'),
+    url(r'^post/(?P<slug>[\w-]+)/$', 'view_post', name='view_post'),
 )
 # Includes static urls settings to main urls
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
